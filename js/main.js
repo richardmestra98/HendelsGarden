ee91771ac445f37ae980fff312eb2874438783fb
richardmestra <richard.mestra98@gmail.com>
2018-06-27 18:22:03 -0500

    Slider Nuevo+Parrallax

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/*::::::::::::::::::// Iniciacion del efecto parallax //:::::::::::::::::::*/
+$('.img-parallax').each(function(){
+    var img = $(this);
+    var imgParent = $(this).parent();
+    function parallaxImg () {
+      var speed = img.data('speed');
+      var imgY = imgParent.offset().top;
+      var winY = $(this).scrollTop();
+      var winH = $(this).height();
+      var parentH = imgParent.innerHeight();
+  
+      // The next pixel to show on screen      
+      var winBottom = winY + winH;
+  
+      // If block is shown on screen
+      if (winBottom > imgY && winY < imgY + parentH) {
+        // Number of pixels shown after block appear
+        var imgBottom = ((winBottom - imgY) * speed);
+        // Max number of pixels until block disappear
+        var imgTop = winH + parentH;
+        // Porcentage between start showing until disappearing
+        var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+      }
+      img.css({
+        top: imgPercent + '%',
+        transform: 'translate(-50%, -' + imgPercent + '%)'
+      });
+    }
+    $(document).on({
+      scroll: function () {
+        parallaxImg();
+      }, ready: function () {
+        parallaxImg();
+      }
+    });
+  });$('.img-parallax').each(function(){
+  var img = $(this);
+  var imgParent = $(this).parent();
+  function parallaxImg () {
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
+
+
+    // The next pixel to show on screen      
+    var winBottom = winY + winH;
+
+    // If block is shown on screen
+    if (winBottom > imgY && winY < imgY + parentH) {
+      // Number of pixels shown after block appear
+      var imgBottom = ((winBottom - imgY) * speed);
+      // Max number of pixels until block disappear
+      var imgTop = winH + parentH;
+      // Porcentage between start showing until disappearing
+      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+    }
+    img.css({
+      top: imgPercent + '%',
+      transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+  }
+  $(document).on({
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
+    }
+  });
+});
+/*::::::::::::::::::// FIn del efecto parallax //:::::::::::::::::::*/
+
+/*:::::::::::::::::::::::::// Efecto Slider //::::::::::::::::::::::::::::::*/
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+/*-----------// Customise //----------- */
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 300,            // Integer: Speed of the transition, in milliseconds
+  timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+
+
+/*:::::::::::::::::::::// Fin Efecto Slider //:::::::::::::::::::::::::::::*/
