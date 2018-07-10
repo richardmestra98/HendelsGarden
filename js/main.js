393b2515bfb6ba91c21fa0a5b8f72cf74edbd62b
richardmestra <richard.mestra98@gmail.com>
2018-07-10 13:04:02 -0500

    Heand editado

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ $(".rslides").responsiveSlides({
   nav: false,             // Boolean: Show navigation, true or false
   random: false,          // Boolean: Randomize the order of the slides, true or false
   pause: false,           // Boolean: Pause on hover, true or false
-  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  pauseControls: false,    // Boolean: Pause when hovering controls, true or false
   prevText: "Previous",   // String: Text for the "previous" button
   nextText: "Next",       // String: Text for the "next" button
   maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
